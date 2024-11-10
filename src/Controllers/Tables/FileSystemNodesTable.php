e1b067f965ca7e3d97480c250e668a3f75a436e7
Abhishek Sharma <abhishek3.dev@gmail.com>
2024-11-10 16:53:37 +0530

    Define file types constants

--- src/Controllers/Tables/FileSystemNodesTable.php
+++ src/Controllers/Tables/FileSystemNodesTable.php
@@ -10,8 +10,6 @@ use DupChallenge\Interfaces\TableInterface;
  */
 class FileSystemNodesTable implements TableInterface
 {
-
-	// Use trait to implement singleton pattern
 	use SingletonTrait;
 
 	/**
@@ -22,12 +20,25 @@ class FileSystemNodesTable implements TableInterface
 	const COLUMN_TYPE = 'type';
 	const COLUMN_SIZE = 'size';
 	const COLUMN_NODE_COUNT = 'node_count';
+	const COLUMN_LAST_MODIFIED = 'last_modified';
 	const COLUMN_LAST_SCANNED = 'last_scanned';
 
 	/**
 	 * Table name
 	 */
 	const TABLE_NAME = 'dup_file_system_nodes';
+
+	/**
+     * File types
+     */
+    const FILE_TYPE_FILE = 'file';
+    const FILE_TYPE_DIR = 'dir';
+    const FILE_TYPE_LINK = 'link';
+    const FILE_TYPE_BLOCK = 'block';
+    const FILE_TYPE_FIFO = 'fifo';
+    const FILE_TYPE_CHAR = 'char';
+    const FILE_TYPE_SOCKET = 'socket';
+    const FILE_TYPE_UNKNOWN = 'unknown';
 
 	/**
 	 * @inheritDoc
@@ -46,10 +57,11 @@ class FileSystemNodesTable implements TableInterface
 	{
 		return [
 			self::COLUMN_ID => 'INT UNSIGNED NOT NULL AUTO_INCREMENT',
-			self::COLUMN_PATH => 'VARCHAR(255) NOT NULL',
-			self::COLUMN_TYPE => 'ENUM("file", "dir") NOT NULL',
-			self::COLUMN_SIZE => 'BIGINT UNSIGNED NOT NULL DEFAULT 0',
+			self::COLUMN_PATH => 'VARCHAR(255) NOT NULL UNIQUE',
+			self::COLUMN_TYPE => 'ENUM(\'' . implode("','", self::getFileTypes()) . '\') DEFAULT \'' . self::FILE_TYPE_UNKNOWN . '\'',
+			self::COLUMN_SIZE => 'BIGINT UNSIGNED DEFAULT 0',
 			self::COLUMN_NODE_COUNT => 'INT UNSIGNED DEFAULT 1',
+			self::COLUMN_LAST_MODIFIED => 'DATETIME DEFAULT NULL',
 			self::COLUMN_LAST_SCANNED => 'DATETIME DEFAULT CURRENT_TIMESTAMP',
 		];
 	}
@@ -67,6 +79,25 @@ class FileSystemNodesTable implements TableInterface
 	 */
 	public function getForeignKey()
 	{
-		return null;
+		return null; // No foreign keys for this table
 	}
+
+	/**
+     * Retrieve the available file types.
+     *
+     * @return array The available file types
+     */
+    private static function getFileTypes(): array
+    {
+        return [
+            self::FILE_TYPE_FILE,
+            self::FILE_TYPE_DIR,
+            self::FILE_TYPE_LINK,
+            self::FILE_TYPE_BLOCK,
+            self::FILE_TYPE_FIFO,
+            self::FILE_TYPE_CHAR,
+            self::FILE_TYPE_SOCKET,
+            self::FILE_TYPE_UNKNOWN,
+        ];
+    }
 }
