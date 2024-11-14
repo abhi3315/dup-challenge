a7d88ab5e722217cda37a7b9d586ea37dc067150
Abhishek Sharma <abhishek3.dev@gmail.com>
2024-11-14 14:58:25 +0530

    Add data definition

---
+++ assets/definition.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Window definition
+ */
+declare interface Window {
+	/**
+	 * Duplicator Challenge Plugin
+	 */
+	dupChallengeData: {
+		restRootUrl: string;
+		rootDir: string;
+		nonce: string;
+		currentPage: string;
+	};
+}
